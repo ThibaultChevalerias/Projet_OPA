0291e86392cd57b365b35a6ca44e2c211bdb681c
3B-night <fgmp1992@yahoo.fr>
2014-11-17 15:36:14 +0100

    added isFlat function to functions annexe (and getMax, getMin and getMean)

--- src_Wang_Landau/fonctionsAnnexes.cpp
+++ src_Wang_Landau/fonctionsAnnexes.cpp
@@ -20,3 +20,79 @@ int locateBin(double deltaE, double energy)
 {
     return floor(energy / deltaE); // euclidian division of energy by deltaE to find in which bin "energy" is
 }
+
+int getMax(int *table, int size) // It only works for integer tables
+{
+    double max = *table[0];
+
+    for(int i = 1; i < size; i++)
+    {
+        if(max < *table[i])
+        {
+            max = *table[i];
+        }
+    }
+    return max;
+
+}
+
+int getMin(int *table, int size) // It only works for integer tables
+{
+    double min = *table[0];
+
+    for(int i = 1; i < size; i++)
+    {
+        if(min > *table[i])
+        {
+            min = *table[i];
+        }
+    }
+    return min;
+
+}
+
+double getMean(int *table, int size) // /!\ eliminates the 0 values in the table from the computation
+{
+    double mean = 0;
+    int count = 0;
+
+    for(int i = 0; i < size; i++)
+    {
+        if(*table[i] != 0)
+        {
+            mean += table[i];
+            count++;
+        }
+    }
+    if(count != 0)
+    {
+        mean /= count;
+        return mean;
+    }
+    else
+    {
+        cout << "bad energy window, energy out of range" << endl;
+        return 0;
+    }
+}
+
+
+bool isFlat(double flatness_limit, int *visits, int size) // It only works for integer tables
+{
+    double mean = 0;
+    double range = 0;
+
+    mean = getMean(visits,size);
+
+    range = getMax(visits,size) - getMin(visits,size);
+
+    if(1-(range/mean) > flatness_limit)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
--- src_Wang_Landau/fonctionsAnnexes.h
+++ src_Wang_Landau/fonctionsAnnexes.h
@@ -3,5 +3,9 @@
 
 int mod(int a, int b);
 int locateBin(double deltaE, double energy);
+int getMax(int *table, int size);
+int getMin(int *table, int size)
+double getMean(int *table, int size);
+bool isFlat(double flatness_limit, int *visits, int size);
 
 #endif // FONCTIONSANNEXES_H_INCLUDED
