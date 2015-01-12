6af187e4967b35e75ca654bc79a694373ebf7561
Thibault Chevalérias <tibochev@gmail.com>
2015-01-12 10:28:57 +0100

    News in convergence code

--- src_energy_minimization_old/main.cpp
+++ src_energy_minimization_old/main.cpp
@@ -14,38 +14,37 @@ int main()
     /* Choice between FM and AFM study */
     int FM_AFM_choice = 0;
 
-    while (FM_AFM_choice != 1 && FM_AFM_choice != 2 && FM_AFM_choice != 3 && FM_AFM_choice != 4) // While the chosen number is not 1 nor 2 nor 3 nor 4
+    while(FM_AFM_choice != 1 && FM_AFM_choice != 2) // While the chosen number is not 1 nor 2
     {
-        cout << "Please choose between a FM or AFM study" << endl << "1. AFM" << endl << "2. FM" << endl << "3. normal" << endl
-        << "4. energy convergence verification --> !!! WARNING !!!: it makes no sense to have more than one temperature step for this one" << endl;
+        cout << "Please choose between a FM or AFM study" << endl << "1.AFM" << endl << "2.FM" << endl;
         cin >> FM_AFM_choice;
     }
 
     cout << "Wait while the simulation is running..." << endl;
 
     /* Déclaration des variables */
-    int nx = 12;
-    int ny = 12;
-    int nz = 12;
+    int nx = 4;
+    int ny = 4;
+    int nz = 8;
 
     double T = 1; //Temperature
     double J0 = 1;
     double J1 = 1; // constante d'échange entre plus proches voisins
     double J2init = - 0.1; // constante d'échange entre seconds voisins
     double J2final = - 1;
-    double J2step = 0.2;
+    double J2step = 0.1;
     
     int sweep = 0;
-    int sweep_max = 10000; // Noumber of Monte-Carlo sweeps
+    int sweep_max = 10000; // Nombre de sweep de simulation
     int x = 0;
     int y = 0;
     int z = 0;
     
     double nombreEntre0Et1 = 0;
     double floattemp = 0;
-    double temperatureStep = 0.05 * 1000;
-    double Tinit = 15.5;
-    double Tinf = 0.1;
+    double temperatureStep = 0.1;
+    double Tinit = 15;
+    double Tinf = 1;
     int counterT = 0; // see far below where this counter is used
 
     /* Variables for output*/
@@ -78,8 +77,8 @@ int main()
     
 
     /* NOUVELLE SECTION : BOUCLE SUR J2/J1 */
-    double estimated_time_minutes = (J2init - J2final) * (Tinit - Tinf) * sweep_max * nx * ny * nz / (35000000 *  temperatureStep * J2step);
-    cout << "Estimated time for normal run : " << estimated_time_minutes << " minutes, or " << estimated_time_minutes / 60 << " hours" << endl;
+    double estimated_time_minutes = (J2init - J2final) * (Tinit - Tinf) * sweep_max/(1000000 * 100 * temperatureStep * J2step);
+    cout << "Estimated time : " << estimated_time_minutes << " minutes, or " << estimated_time_minutes / 60 << " hours" << endl;
 
     for(double J2 = J2init; J2 >= J2final; J2 -= J2step)
     {
@@ -98,7 +97,6 @@ int main()
         string const plot_file("results/results" + label);
         string const Cv_file("results/data/Cv" + label + ".dat");
         string const chi_file("results/data/chi" + label + ".dat");
-        string const energy_file("results/data/energy" + label + ".dat");        
         string const sumMagnetizationPlanes_file("results/data/sumMagnetizationPlanes" + label + ".dat");
         string const AFMcriterium_file("results/data/AFMcriterium" + label + ".dat");
         string const magnetization_file("results/data/magnetization" + label + ".dat");
@@ -107,7 +105,6 @@ int main()
         ofstream plot_stream(plot_file.c_str());
         ofstream Cv_stream(Cv_file.c_str());
         ofstream chi_stream(chi_file.c_str());
-        ofstream energy_stream(energy_file.c_str());
         ofstream sumMagnetizationPlanes_stream(sumMagnetizationPlanes_file.c_str());
         ofstream AFMcriterium_stream(AFMcriterium_file.c_str());
         ofstream magnetization_stream(magnetization_file.c_str());
@@ -115,58 +112,33 @@ int main()
 
         if(FM_AFM_choice == 1) //AFM
         {
-            /* We select the useful streams (the streams have to be declared outside the 'if') */
+            /* We select the useful streams (the streams have to be declared outside the 'if'*/
             magnetization_stream.close();
 
-            streams = sumMagnetizationPlanes_stream && AFMcriterium_stream && Cv_stream && chi_stream && energy_stream && plot_stream && configuration_stream;
+            streams = sumMagnetizationPlanes_stream && AFMcriterium_stream && Cv_stream && chi_stream && plot_stream && configuration_stream;
         }
-        else if(FM_AFM_choice == 2)
+        else //FM (we assume FM_AFM_choice ==2)
         {
             sumMagnetizationPlanes_stream.close();
             AFMcriterium_stream.close();
 
-            streams = magnetization_stream && Cv_stream && chi_stream && energy_stream && plot_stream && configuration_stream;
-        }
-        else if(FM_AFM_choice == 3)
-        {
-            magnetization_stream.close();
-            sumMagnetizationPlanes_stream.close();
-            AFMcriterium_stream.close();
-
-            streams = Cv_stream && chi_stream && energy_stream && plot_stream && configuration_stream;
-        }
-        else // we assume FM_AFM_choice == 4
-        {
-            magnetization_stream.close();
-            sumMagnetizationPlanes_stream.close();
-            AFMcriterium_stream.close();
-            Cv_stream.close();
-            chi_stream.close();
-            plot_stream.close();
-            configuration_stream.close();
-            energy_stream.close();
-            
-            streams = 1;
+            streams = magnetization_stream && Cv_stream && chi_stream && plot_stream && configuration_stream;
         }
 
         if(streams)
         {
-            /* Headers */
-            if(FM_AFM_choice != 4)
-            {
-                plot_stream << "#Parameters : J0 = J1 = " << J1 << "; J2 = " << J2 << "; number of sweeps = " << sweep_max << endl;
-                Cv_stream << "#Temperature Cv" << endl;
-                chi_stream << "#Temperature chi" << endl;
-                configuration_stream << "#T z sumMagnetization" << endl;
-                energy_stream << "#Temperature Energy" << endl;
-            }
-            
+            /* Entêtes */
+            plot_stream << "#Parameters : J0 = J1 = " << J1 << "; J2 = " << J2 << "; number of sweeps = " << sweep_max << endl;
+            Cv_stream << "#Temperature Cv" << endl;
+            chi_stream << "#Temperature chi" << endl;
+            configuration_stream << "#T z sumMagnetization" << endl;
+
             if(FM_AFM_choice == 1) //AFM
             {
                 sumMagnetizationPlanes_stream << "#Temperature sumMagnetizationPlanes" << endl;
                 AFMcriterium_stream << "#Temperature AFMcriterium" << endl;
             }
-            else if(FM_AFM_choice == 2)
+            else //FM (we assume FM_AFM_choice ==2)
             {
                 magnetization_stream << "#Temperature magnetization" << endl;
             }
@@ -176,16 +148,6 @@ int main()
             /* REMARQUE : FONCTION SEPAREE A FAIRE, CLAIREMENT ! */
             for(T = Tinit; T >= Tinf; T -= temperatureStep)
             {
-                // What we had not done earlier for FM_AFM_choice == 4:
-                string const convergence_file = "results/convergence" + label + "_T=" + to_string(T) + ".dat";
-                ofstream convergence_stream(convergence_file.c_str());
-                convergence_stream << "#Sweep Energy" << endl;
-                
-                if(FM_AFM_choice != 4)
-                {
-                    convergence_stream.close();
-                }
-                
                 cout << "J2 : " << J2 << "; T : " << T << endl;
                 counterT ++;
 
@@ -210,11 +172,11 @@ int main()
                 sum_AFMcriterium = 0;
                 DeltaAFMcriterium = 0;
 
-//                for(int i = 0; i < nz; i++)
-//                {
-//                    configuration[i] = 0;
-//                    sum_configuration[i] = 0;
-//                }
+                for(int i = 0; i < nz; i++)
+                {
+                    configuration[i] = 0;
+                    sum_configuration[i] = 0;
+                }
                 
                 /********************************************/
                 /********** Algorithme en lui-même **********/
@@ -249,13 +211,8 @@ int main()
                             } //end for z
                         } // end for y
                     } // end for x
-                    
-                    if(FM_AFM_choice == 4)
-                    {
-                        convergence_stream << sweep << " " << States.getEnergy(J0, J1, J2) << endl;
-                    }
-                    
-                    if(sweep >= sweep_max/2 && FM_AFM_choice != 4) // We keep the final half of the states to compute the averaged variables
+                        
+                    if(sweep >= sweep_max/2) // We keep the final half of the states to compute the averaged variables
                     {
                         E = States.getEnergy(J0, J1, J2);
                         sum_E += E;
@@ -274,47 +231,45 @@ int main()
                             sum_AFMcriterium += AFMcriterium;
                         }
 
-//                        for(int i = 0; i < nz; i++)
-//                        {
-//                            configuration[i] = States.getMagnetizationPlane(i);
-//                            sum_configuration[i] += configuration[i];
-//                        }
+                        for(int i = 0; i < nz; i++)
+                        {
+                            configuration[i] = States.getMagnetizationPlane(i);
+                            sum_configuration[i] += configuration[i];
+                        }
                     }
 
                 }//end for sweep_max
 
                 /* Writing results */
-                
-                if(FM_AFM_choice != 4)
-                {
-                    /* Cv = (1/kT^2) * (<E^2> - <E>^2) */
-                    Cv = (2/(sweep_max*T*T)) * (sum_E_square - sum_E * sum_E * 2 / sweep_max);
-                    /* chi = (1/kT) * (<M^2> - <M>^2) */
-                    chi = (2/(sweep_max*T)) * (sum_M_square - sum_M * sum_M * 2 / sweep_max);
 
-                    Cv_stream << T << " " << Cv << endl;
-                    chi_stream << T << " " << chi << endl;
-                    energy_stream << T << " " << sum_E * 2 / sweep_max << endl;
-                }
+                /* Cv = (1/kT^2) * (<E^2> - <E>^2) */
+                Cv = (2/(sweep_max*T*T)) * (sum_E_square - sum_E * sum_E * 2 / sweep_max);
+
+                /* chi = (1/kT) * (<M^2> - <M>^2) */
+                chi = (2/(sweep_max*T)) * (sum_M_square - sum_M * sum_M * 2 / sweep_max);
+
+                Cv_stream << T << " " << Cv << endl;
+                chi_stream << T << " " << chi << endl;
+
                 if(FM_AFM_choice == 1) //AFM
                 {
                     sumMagnetizationPlanes_stream << T << " " << 2 * sum_sumMagnetizationPlanes / sweep_max << endl;
                     AFMcriterium_stream << T << " " << 2 * sum_AFMcriterium / sweep_max << endl;
                 }
-                else if(FM_AFM_choice == 2)
+                else //FM (we assume FM_AFM_choice ==2)
                 {
                     magnetization_stream << T << " " << abs(2 * sum_M / sweep_max) << endl;
                 }
 
                 /* We only plot for a few values of T so that the graph is not too ugly */
-//                if(counterT % 2 == 0)
-//                {
-//                    for(int i = 0; i < nz; i++)
-//                    {
-//                        configuration_stream << T << " " << i << " " << abs(2 * sum_configuration[i] / sweep_max) << endl;
-//                    }
-//                    configuration_stream << endl;
-//                }
+                if(counterT % 2 == 0)
+                {
+                    for(int i = 0; i < nz; i++)
+                    {
+                        configuration_stream << T << " " << i << " " << abs(2 * sum_configuration[i] / sweep_max) << endl;
+                    }
+                    configuration_stream << endl;
+                }
 
             }//end for T
 
@@ -328,29 +283,25 @@ int main()
                 sumMagnetizationPlanes_stream.close();
                 AFMcriterium_stream.close();
             }
-            else if(FM_AFM_choice == 2)
+            else //FM (we assume FM_AFM_choice == 2)
             {
                 plot_stream << "set ylabel 'Total Magnetization'" << endl 
                     << "plot 'data/magnetization" + label + ".dat'" << endl << "pause -1" << endl;
 
                 magnetization_stream.close();
             }
-            
-            if(FM_AFM_choice != 4)
-            {
-                plot_stream << "set ylabel 'Cv, chi'" << endl 
-                    << "plot 'data/Cv" + label + ".dat', 'data/chi" + label + ".dat'" << endl << "pause -1" << endl 
-                    << "set ylabel 'energy'" << endl
-                    << "plot 'data/energy" + label + ".dat'" 
-    //                << "set ylabel 'spin position along z'" << endl << "set zlabel 'magnetization of the plane at height z'" << endl
-    //                << "splot [" << Tinf << ":" << Tinit << "] [0:" << nz - 1 << "] [0:" << nx*ny << "] 'data/configuration" + label + ".dat' with lines"
-                    << endl << "pause -1" << endl;
 
-                Cv_stream.close();
-                chi_stream.close();
-                configuration_stream.close();
-                plot_stream.close();
-            }
+            plot_stream << "set ylabel 'Cv, chi'" << endl 
+                << "plot 'data/Cv" + label + ".dat', 'data/chi" + label + ".dat'" << endl << "pause -1" << endl 
+                << "set ylabel 'spin position along z'" << endl << "set zlabel 'magnetization of the plane at height z'" << endl
+                << "splot [" << Tinf << ":" << Tinit << "] [0:" << nz - 1 << "] [0:" << nx*ny << "] 'data/configuration" + label + ".dat' with lines"
+                << endl << "pause -1" << endl;
+
+            Cv_stream.close();
+            chi_stream.close();
+            configuration_stream.close();
+            plot_stream.close();
+
 //            system("start C:\\Users\\Thibault\\Desktop\\Projet_OPA\\gnuplot\\bin\\gnuplot.exe");
 
         }//end if(streams)
@@ -358,7 +309,7 @@ int main()
         {
             cout << "Error while opening files" << endl;
         }
-    } // end for J2
+    }
 
     return 0;
 }
