72f5f63572412e03b6ca07017fd5cc463786bc2c
3B-night <fgmp1992@yahoo.fr>
2015-01-19 11:04:37 +0100

    modified Wang Landau
    Parcours aléatoire des spins à retourner au lieu de sweeps systématiques

---
+++ src_Wang_Landau_random/fonctionsAnnexes.h
@@ -0,0 +1,15 @@
+#ifndef FONCTIONSANNEXES_H_INCLUDED
+#define FONCTIONSANNEXES_H_INCLUDED
+
+#include <vector>
+
+using namespace std;
+
+int mod(int a, int b);
+int locateBin(double E_min, double deltaE, double energy);
+int getMax(vector<int>& table);
+int getMin(vector<int>& table);
+double getMean(vector<int>& table);
+bool isFlat(double flatness_limit, vector<int>& visits);
+
+#endif // FONCTIONSANNEXES_H_INCLUDED
